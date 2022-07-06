ae3dd6b4a0ecf6d9e22c052560b113a08d088bdf
Leenqq <eugeneverbitskiy84@gmail.com>
2022-07-06 12:38:01 +0300

    Add a view for the main page of the application

---
+++ TestAssignment/Views/StarterPageView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TestAssignment.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для StarterPageView.xaml
+    /// </summary>
+    public partial class StarterPageView : UserControl
+    {
+        public StarterPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
