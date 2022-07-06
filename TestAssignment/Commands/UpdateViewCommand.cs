d939f87bc26a27aee175c7d658f407ddf07669bf
Leenqq <eugeneverbitskiy84@gmail.com>
2022-07-06 13:19:17 +0300

    Add UpdateViewCommand file, that is responsible for navigation

---
+++ TestAssignment/Commands/UpdateViewCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+using TestAssignment.ViewModels;
+
+namespace TestAssignment.Commands
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private MainViewModel viewModel;
+
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(parameter.ToString() == "Main")
+            {
+                viewModel.SelectedViewModel = new StarterPageViewModel();
+            }
+            else if(parameter.ToString() == "Currency")
+            {
+                viewModel.SelectedViewModel = new DetailPageViewModel();
+            }
+        }
+    }
+}
