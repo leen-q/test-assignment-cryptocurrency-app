a419509d1f6aaa7f79b578d154493c954d594fab
Leenqq <eugeneverbitskiy84@gmail.com>
2022-07-06 13:21:46 +0300

    Add StarterViewModel

---
+++ TestAssignment/ViewModels/StarterPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using TestAssignment.Models;
+
+namespace TestAssignment.ViewModels
+{
+    public class StarterPageViewModel : BaseViewModel
+    {
+        public IList<Currency> CurrencyList { get; set; }
+        public StarterPageViewModel()
+        {
+            string json = new WebClient().DownloadString("https://api.coincap.io/v2/assets?limit=10");
+            CurrenciesList list = JsonConvert.DeserializeObject<CurrenciesList>(json);
+            CurrencyList = list.data;
+        }
+    }
+}
