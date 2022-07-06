51bcf1f02c51300c79e0729b36741bde1ad354b1
Leenqq <eugeneverbitskiy84@gmail.com>
2022-07-06 13:20:50 +0300

    Add ValueConverter file for design purposes

---
+++ TestAssignment/Converter/ValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TestAssignment.Converter
+{
+    public class ValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (decimal)value >= 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
