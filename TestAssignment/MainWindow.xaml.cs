89069288d03cb27d2c11c0feef475f4f00e2d3a2
Leenqq <eugeneverbitskiy84@gmail.com>
2022-07-06 13:17:15 +0300

    Initial commit

---
+++ TestAssignment/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using TestAssignment.ViewModels;
+
+namespace TestAssignment
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+    }
+}
