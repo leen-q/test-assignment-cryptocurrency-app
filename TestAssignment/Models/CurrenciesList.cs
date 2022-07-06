bbc2cd5325a16e41f3f244ef89a9b802330550b6
Leenqq <eugeneverbitskiy84@gmail.com>
2022-07-06 12:34:52 +0300

    Add Currency and CyrrenciesList models

---
+++ TestAssignment/Models/CurrenciesList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TestAssignment.Models
+{
+    public class CurrenciesList
+    {
+        public IList<Currency> data { get; set; }
+        public long timestamp { get; set; }
+    }
+}
---
+++ TestAssignment/Models/Currency.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAssignment.Models
+{
+    public class Currency
+    {
+        public string id { get; set; }
+        public int? rank { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public decimal? volumeUsd24Hr { get; set; }
+        public decimal? priceUsd { get; set; }
+        public decimal? changePercent24Hr { get; set; }
+
+    }
+}
