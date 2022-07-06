f342383e85d1c2d91c12daeee505278235bd13c1
Leenqq <eugeneverbitskiy84@gmail.com>
2022-07-06 13:21:19 +0300

    Add MainViewModel

---
+++ TestAssignment/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Input;
+using TestAssignment.Commands;
+
+namespace TestAssignment.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public ICommand UpdateViewCommand { get; set; }
+
+        private BaseViewModel _selectedViewModel = new StarterPageViewModel();
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set 
+            { 
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+    }
+}
