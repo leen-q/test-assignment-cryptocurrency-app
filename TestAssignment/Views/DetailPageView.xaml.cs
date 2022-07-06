f896d4f7809e629d097f7171749986e676f6adf0
Leenqq <eugeneverbitskiy84@gmail.com>
2022-07-06 12:39:31 +0300

    Add a view for the page with detailed information about the cryptocurrencies

---
+++ TestAssignment/Views/DetailPageView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Controls;
+using TestAssignment.Models;
+using TestAssignment.ViewModels;
+
+namespace TestAssignment.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для DetailPageView.xaml
+    /// </summary>
+    public partial class DetailPageView : UserControl
+    {
+        DetailPageViewModel viewModel;
+        public DetailPageView()
+        {
+            InitializeComponent();
+            viewModel = new DetailPageViewModel();
+            DataContext = viewModel;
+            CurrencyView.Items.Filter = CurrencyFinder;
+        }
+
+        private void CurrencyView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Currency selectedCurrency = (Currency)CurrencyView.SelectedItem;
+            if(selectedCurrency != null)
+            {
+                viewModel.GetMarket(selectedCurrency);
+                MarketView.ItemsSource = viewModel.MarketList;
+            }
+        }
+
+        private bool CurrencyFinder(object obj)
+        {
+            var findCurrency = obj as Currency;
+
+            return findCurrency.name.ToLower().Contains(FindCurrency.Text.ToLower());
+        }
+
+        private void FindCurrency_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FindCurrency.Text == null)
+            {
+                CurrencyView.Items.Filter = null;
+            }
+            else
+            {
+                CurrencyView.Items.Filter = CurrencyFinder;
+            }
+        }
+    }
+}
